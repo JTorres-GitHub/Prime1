525755b0b149bbd7e830106243a7dff07c11aa75
Joaquin.Torres <torres.joaquin@gmail.com>
2020-06-12 10:01:46 -0400

    Initial code

---
+++ Prime1/src/Prime1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Prime1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+		boolean flag = false;
+		int i;
+		
+		// integer input
+		System.out.println("Enter an integer: ");
+		num = input.nextInt();
+		
+		// prime check
+		for (i = 2; i <= num/2; ++i){
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		
+		if (!flag)
+			System.out.println(num + " is a prime number.");
+		else
+			System.out.println(num + " is not a prime number.");
+		
+	}
+}
